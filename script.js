9760859855e3a9c94fc5182ff6ae0f85008b43ec
Tim_Winfred <twinfred@gmail.com>
2018-03-28 21:46:39 -0700

    created some css and started the html framework

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     for(var i = 1; i<=802; i++) {
         console.log(i);
-        $('body').append('<img src="http://pokeapi.co/media/sprites/pokemon/'+i+'.png" alt="pokemon number '+i+'">');
+        $('#wrapper').append('<img src="http://pokeapi.co/media/sprites/pokemon/'+i+'.png" alt="pokemon number '+i+'">');
     }
 })
